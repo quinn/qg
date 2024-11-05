afd530326ce4d98cc0940f32bef839575b700f0b
quinn <q.shanahan@gmail.com>
2024-11-05 13:21:29 -0500

    ai wip

---
+++ config/types.go
@@ -0,0 +1,26 @@
+package config
+
+// Config represents the main configuration structure
+type Config struct {
+	Version    string      `yaml:"version"`
+	Generators []Generator `yaml:"generators"`
+}
+
+// Generator represents each generator in the generators list
+type Generator struct {
+	Name       string              `yaml:"name"`
+	Args       []string            `yaml:"args"`
+	Transforms []map[string]string `yaml:"transforms"`
+	Use        []string            `yaml:"use"`
+	Post       []string            `yaml:"post"`
+}
+
+// FindGenerator returns the generator with the given name
+func (c Config) FindGenerator(gName string) (Generator, error) {
+	for _, g := range c.Generators {
+		if gName == g.Name {
+			return g, nil
+		}
+	}
+	return Generator{}, fmt.Errorf("generator not found: %s", gName)
+}
